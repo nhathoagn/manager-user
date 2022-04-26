32d78081f487b785fc22680c1f6b8134a47379f8
nhathoang <nguyennhathoang35@gmail.com>
2022-04-26 14:34:47 +0700

    show button, form using state

--- src/Components/AddUser/AddUser.js
+++ src/Components/AddUser/AddUser.js
@@ -1,38 +1,70 @@
 import React from "react";
 
 class AddUser extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isShow : false
+		}
+	}
+	showButton = () =>{
+		if (this.state.isShow === true){
+			return 	<div className="btn btn-block btn-outline-secondary" onClick={ () => this.hideButton()}>Đóng lại</div>
+		}else {
+			return <div className="btn btn-block btn-outline-info" onClick={ () => this.hideButton()}>Thêm Mới</div>
+		}
+	}
+	hideButton = () =>{
+		this.setState({
+			isShow: !this.state.isShow
+		});
+	}
+        showForm= () => {
+	        if (this.state.isShow === true){
+		        return(
+
+
+
+
+				        <div className="card mt-3">
+
+
+					        <div className="card-header ">Thêm Mới </div>
+					        <div className="card-body text-primary ">
+
+						        <div className="form-group">
+							        <input type="text" className="form-control"  aria-describedby="helpId" placeholder="Tên User" />
+						        </div>
+						        <div className="form-group">
+							        <input type="text" className="form-control"  aria-describedby="helpId" placeholder="Số Điện Thoại" />
+						        </div>
+						        <div className="form-group">
+							        <select className="custom-select" required>
+								        <option >Chọn Quyền</option>
+								        <option value={1}>Admin</option>
+								        <option value={2}>Normal</option>
+							        </select>
+						        </div>
+						        <div className="form-group">
+							        <div className="btn btn-block btn-primary">Thêm Mới</div>
+						        </div>
+					        </div>
+				        </div>
+
+		        )
+	        }
+        }
 	render() {
+
 		return (
-			<div>
-				<div className="col-3">
-					.
-					<div className="card border-primary">
-						<div className="card-header">Thêm mới </div>
-						<div className="card-body text-primary">
-							<h5 className="card-title">
-							</h5>
-							<div className="form-group">
-								<input type="text" className="form-control" name aria-describedby="helpId" placeholder="Tên User" />
-							</div>
-							<div className="form-group">
-								<input type="text" className="form-control" name aria-describedby="helpId" placeholder="Số Điện Thoại" />
-							</div>
-							<div className="form-group">
-								<select className="custom-select" required>
-									<option selected>Chọn Quyền</option>
-									<option value={1}>Admin</option>
-									<option value={2}>Normal</option>
-								</select>
-							</div>
-							<div className="form-group">
-								<div className="btn btn-block btn-primary">Thêm Mới</div>
-							</div>
-						</div>
-					</div>
-				</div>
-
-
+			<div className="col-3 right">
+				{this.showButton()}
+				{this.showForm()}
 			</div>
+
+
+
 		);
 	}
 }
+export default AddUser
